d129a2837c61aa7eaf9dd041e4f2bd335bc6a5a1
SaiBou <bouamarsaid@hotmail.fr>
2024-09-06 16:07:02 +0200

    updatefeature

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 import NavBar from './NavBar'; // Assumes NavBar is in a separate file or defined above
 import HeroSection from './HeroSection'; // Assumes HeroSection is either defined above or imported
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/HomePage';
 
 const App = () => {
   return (
-    <div>
-      <NavBar />
-      <HeroSection />
-    </div>
+    <Router>
+      <div>
+        {/* Définir les routes pour les composants */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
